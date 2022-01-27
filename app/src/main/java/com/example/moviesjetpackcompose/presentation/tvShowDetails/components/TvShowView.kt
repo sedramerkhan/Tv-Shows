f4e27603c1e0211f9cee55f7de18973084075204
Sedra Merkhan <sedramerkhan@gmail.com>
2022-01-27 12:58:33 +0200

    use constrainLayout && delete Box

--- app/src/main/java/com/example/moviesjetpackcompose/presentation/tvShowDetails/components/TvShowView.kt
+++ app/src/main/java/com/example/moviesjetpackcompose/presentation/tvShowDetails/components/TvShowView.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 @Composable
 fun TvShowView(
     tvShow: TvShowDetails,
-){
+) {
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
@@ -32,79 +32,65 @@ fun TvShowView(
         item {
             Column {
 
-
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(430.dp)
-                ) {
-                    ConstraintLayout(Modifier.fillMaxWidth()) {
+                ConstraintLayout(Modifier.fillMaxWidth()) {
 
 
-                        val (imageRef, nameRef,networkRef,dateRef,statusRef) = createRefs()
-                        CoilImage(
-                            link = tvShow.pictures[0],
-                            modifier = Modifier .constrainAs(imageRef){
-                                top.linkTo(parent.top)
-                                end.linkTo(parent.end)
-                                start.linkTo(parent.start)
-                            },
-                            imageModifier = Modifier
-                                .height(IMAGE_HEIGHT.dp)
-                                .fillMaxWidth(),
-                            loading = true
-                        )
-                        val startBias = createGuidelineFromStart(170.dp)
-                        Text(text = tvShow.name,
-                            style = MaterialTheme.typography.h3,
-                        modifier = Modifier.constrainAs(nameRef){
-                            top.linkTo(imageRef.bottom)
-                            end.linkTo(imageRef.end)
-                            start.linkTo(startBias)
+                    val (imageRef, thumbnailRef, textRef) = createRefs()
+                    CoilImage(
+                        link = tvShow.pictures[0],
+                        modifier = Modifier.constrainAs(imageRef) {
+                            top.linkTo(parent.top)
+                            end.linkTo(parent.end)
+                            start.linkTo(parent.start)
 
-                        })
-                        Text(
-                            text = tvShow.network + " (" + tvShow.country + ")",
-                            style = MaterialTheme.typography.h5,
-                            modifier = Modifier.constrainAs(networkRef){
-                                    top.linkTo(nameRef.bottom)
-                                    start.linkTo(startBias)
+                        },
 
-                                })
-
-                        Text(
-                        text = tvShow.status,
-                            style = MaterialTheme.typography.subtitle1,
-                            color = Green300,
-                        modifier = Modifier.constrainAs(statusRef){
-                            top.linkTo(networkRef.bottom)
-                            start.linkTo(startBias)
-                        })
-                        tvShow.start_date?.let {
-//                    var end = tvShow.end_date ?: ""
-                            Text(
-                                text = "Started On: $it",
-                                style = MaterialTheme.typography.h5,
-                                modifier =  Modifier.constrainAs(dateRef){
-                                    top.linkTo(statusRef.bottom)
-                                    start.linkTo(startBias)
-                                }
-                            )
-                        }
-
-                    }
+                        imageModifier = Modifier
+                            .height(IMAGE_HEIGHT.dp)
+                            .fillMaxWidth(),
+                        loading = true
+                    )
                     CoilImage(
                         link = tvShow.image_thumbnail_path,
                         modifier = Modifier
-                            .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 8.dp)
-                            .align(
-                                Alignment.BottomStart
-                            ),
+                            .padding(horizontal = 13.dp)
+                            .constrainAs(thumbnailRef) {
+                                top.linkTo(imageRef.bottom)
+                                bottom.linkTo(imageRef.bottom)
+                                start.linkTo(parent.start)
+                            },
                         imageModifier = Modifier
                             .height(220.dp)
                             .width(140.dp),
                         roundCorner = 20f
                     )
+                    Column(modifier = Modifier.fillMaxWidth().constrainAs(textRef) {
+                            top.linkTo(imageRef.bottom)
+                            start.linkTo(thumbnailRef.end)
+                        }) {
+                        Text(
+                            text = tvShow.name,
+                            style = if(tvShow.name.length <18) MaterialTheme.typography.h3 else if(tvShow.name.length >22) MaterialTheme.typography.subtitle1 else  MaterialTheme.typography.h4,
+                        )
+                        Text(
+                            text = tvShow.network + " (" + tvShow.country + ")",
+                            style = MaterialTheme.typography.h5,
+                        )
+                        Text(
+                            text = tvShow.status,
+                            style = MaterialTheme.typography.subtitle1,
+                            color = Green300,
+                        )
+                        tvShow.start_date?.let {
+//                    var end = tvShow.end_date ?: ""
+                            Text(
+                                text = "Started On: $it",
+                                style = MaterialTheme.typography.h5,
+                            )
+                        }
+
+
+                    }
 
                 }
 
