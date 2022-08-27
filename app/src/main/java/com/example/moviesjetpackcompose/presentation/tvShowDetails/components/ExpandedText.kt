1bc15a7da8111fedb8a914c0ab8f0cd7a25c53d7
Sedra Merkhan <sedramerkhan@gmail.com>
2022-08-27 17:22:05 +0300

    show more only if text lines more than 4

--- app/src/main/java/com/example/moviesjetpackcompose/presentation/tvShowDetails/components/ExpandedText.kt
+++ app/src/main/java/com/example/moviesjetpackcompose/presentation/tvShowDetails/components/ExpandedText.kt
@@ -1,16 +1,16 @@
 package com.example.moviesjetpackcompose.presentation.tvShowDetails.components
 
-import androidx.compose.foundation.background
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
@@ -24,6 +24,9 @@ fun ExpandedText(
         true -> Int.MAX_VALUE to "less"
         false -> 4 to "more"
     }
+    var isLineEllipsized by remember {
+        mutableStateOf(false)
+    }
 
     Text(
         text = description,
@@ -31,19 +34,24 @@ fun ExpandedText(
         modifier = Modifier.padding(horizontal = 16.dp),
         maxLines = maxLines,
         overflow = TextOverflow.Ellipsis,
-        color = MaterialTheme.colors.onSurface
+        color = MaterialTheme.colors.onSurface,
+        onTextLayout = { textLayoutResult: TextLayoutResult ->
+            if (maxLines != Int.MAX_VALUE)
+                isLineEllipsized = textLayoutResult.isLineEllipsized(maxLines - 1)
+        }
     )
-
-    Text(
-        text = buttonText,
-        color = Color.Blue,
-        style = MaterialTheme.typography.subtitle1,
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .padding(bottom = 10.dp)
-            .clickable(
-                onClick = onClick
-            )
-    )
-
+    Log.i("Line", isLineEllipsized.toString())
+    if (isLineEllipsized) {
+        Text(
+            text = buttonText,
+            color = Color.Blue,
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .clickable(
+                    onClick = onClick
+                )
+        )
+    }
+    Spacer(Modifier.height(10.dp))
 }
