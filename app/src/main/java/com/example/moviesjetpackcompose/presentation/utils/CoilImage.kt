3dace6fac5c307205c1610d42a92f94e50fbda81
SedraITE <145347910+SedraITE@users.noreply.github.com>
2025-10-21 15:24:10 +0300

    chore: upgrade kotlin to 2.2.20

--- app/src/main/java/com/example/moviesjetpackcompose/presentation/utils/CoilImage.kt
+++ app/src/main/java/com/example/moviesjetpackcompose/presentation/utils/CoilImage.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest.Builder
 import coil.transform.RoundedCornersTransformation
 import com.example.moviesjetpackcompose.R
 
@@ -24,28 +26,34 @@ fun CoilImage(
     roundCorner: Float = 0f,
     placeholder: Int = R.drawable.placeholder,
     loading: Boolean = false
-    ) {
+) {
     Box(
         modifier = modifier
     ) {
 
-        val painter = rememberImagePainter(
-            data = link,
-            builder = {
-                placeholder(placeholder)
-                error(R.drawable.error)
-                crossfade(1000)
-                transformations(
-                    RoundedCornersTransformation(roundCorner)
-//                    GrayscaleTransformation(),
-//                    CircleCropTransformation(),
-//                    BlurTransformation(LocalContext.current),
-                )
+        val painter = rememberAsyncImagePainter(
+                Builder(LocalContext.current).data(data = link)
+                    .apply(//                    GrayscaleTransformation(),
+                        //                    CircleCropTransformation(),
+                        //                    BlurTransformation(LocalContext.current),
 
-                //this. to know all other functions
-//                press ctrl + q to show function description
-            }
-        )
+                        //this. to know all other functions
+                        //                press ctrl + q to show function description
+                        block = { ->
+                            placeholder(placeholder)
+                            error(R.drawable.error)
+                            crossfade(1000)
+                            transformations(
+                                RoundedCornersTransformation(roundCorner)
+                                //                    GrayscaleTransformation(),
+                                //                    CircleCropTransformation(),
+                                //                    BlurTransformation(LocalContext.current),
+                            )
+
+                            //this. to know all other functions
+                            //                press ctrl + q to show function description
+                        }).build()
+            )
 
         Image(
             painter = painter,
@@ -54,7 +62,7 @@ fun CoilImage(
             modifier = imageModifier
         )
         val painterState = painter.state
-        if (painterState is ImagePainter.State.Loading && loading ){
+        if (painterState is AsyncImagePainter.State.Loading && loading) {
             CircularIndeterminateProgressBar(verticalBias = 0.5f)
         }
     }
